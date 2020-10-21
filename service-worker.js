85475db618bfc4f26d885ac198b6fd28482633eb
Sofiia Kucherenko <kucherenko.sv13@gmail.com>
2020-10-21 12:05:34 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.6de9460da4fc8c038dc9b369e728ed03.js"
 );
 
 self.addEventListener('message', (event) => {
